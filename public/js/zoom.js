c6329f419b94c8ed7ac4d2a3d6bfa2222e0eb77a
Roberto Carlos López Abarca <rcla200031@gmail.com>
2023-05-24 21:19:54 -0600

    Refactorizando código de zoom

--- public/js/zoom.js
+++ public/js/zoom.js
@@ -1,116 +1,118 @@
-const scrollable = document.querySelector('.scrollable');
-const contenedorUbicaciones = document.querySelector('.area-zoom');
-const aumentar = document.querySelector('#aumentar');
-const disminuir = document.querySelector('#disminuir');
-let contador = 0;
-disminuir.disabled = true;
+const zoom = () => {
+    const scrollable = document.querySelector('.scrollable');
+    const contenedorUbicaciones = document.querySelector('.area-zoom');
+    const aumentar = document.querySelector('#aumentar');
+    const disminuir = document.querySelector('#disminuir');
+    let contador = 0;
+    disminuir.disabled = true;
 
-const aumentarZoomConClick = () => {
-    contador += 1;
+    const aumentarZoomConClick = () => {
+        contador += 1;
 
-    switch(contador) {
-        case 1:
-            contenedorUbicaciones.style.transform = 'scale(140%)';
-            disminuir.disabled = false;
-        break;
-        case 2:
-            contenedorUbicaciones.style.transform = 'scale(165%)';
-        break;
-        case 3:
-            contenedorUbicaciones.style.transform = 'scale(195%)';
-            aumentar.disabled = true;
-        break;
-        case 4:
-            contenedorUbicaciones.style.transform = 'scale(100%)';
-            aumentar.disabled = false;
-            disminuir.disabled = true;
-            contador = 0;
-        break;   
-    }
-}
-
-const aumentarZoomConBoton = () => {
-    aumentar.disabled = false;
-    contador += 1;
-
-    switch(contador) {
-        case 1:
-            contenedorUbicaciones.style.transform = 'scale(140%)';
-            disminuir.disabled = false;
-        break;
-        case 2:
-            contenedorUbicaciones.style.transform = 'scale(165%)';
-        break;
-        case 3:
-            contenedorUbicaciones.style.transform = 'scale(195%)';
-            aumentar.disabled = true;
-        break; 
-    }
-}
-
-const disminuirZoomConBoton = () => {
-    disminuir.disabled = false;
-
-    switch(contador) {
-        case 1:
-            contenedorUbicaciones.style.transform = 'scale(100%)';
-            disminuir.disabled = true;
-        break;
-        case 2:
-            contenedorUbicaciones.style.transform = 'scale(140%)';
-        break;
-        case 3:
-            contenedorUbicaciones.style.transform = 'scale(165%)';
-            aumentar.disabled = false;
-        break;
+        switch(contador) {
+            case 1:
+                contenedorUbicaciones.style.transform = 'scale(140%)';
+                disminuir.disabled = false;
+            break;
+            case 2:
+                contenedorUbicaciones.style.transform = 'scale(165%)';
+            break;
+            case 3:
+                contenedorUbicaciones.style.transform = 'scale(195%)';
+                aumentar.disabled = true;
+            break;
+            case 4:
+                contenedorUbicaciones.style.transform = 'scale(100%)';
+                aumentar.disabled = false;
+                disminuir.disabled = true;
+                contador = 0;
+            break;   
+        }
     }
 
-    contador -= 1;
-}
+    const aumentarZoomConBoton = () => {
+        aumentar.disabled = false;
+        contador += 1;
 
-contenedorUbicaciones.addEventListener('dblclick', aumentarZoomConClick);
-aumentar.addEventListener('click', aumentarZoomConBoton);
-disminuir.addEventListener('click', disminuirZoomConBoton);
+        switch(contador) {
+            case 1:
+                contenedorUbicaciones.style.transform = 'scale(140%)';
+                disminuir.disabled = false;
+            break;
+            case 2:
+                contenedorUbicaciones.style.transform = 'scale(165%)';
+            break;
+            case 3:
+                contenedorUbicaciones.style.transform = 'scale(195%)';
+                aumentar.disabled = true;
+            break; 
+        }
+    }
 
-/* * Drag and Scroll */
-let pos = {
-    top: 0,
-    left: 0,
-    x: 0,
-    y: 0
-};
+    const disminuirZoomConBoton = () => {
+        disminuir.disabled = false;
 
-const mouseMoveHandler = (e) => {
-    const dx = e.clientX - pos.x;
-    const dy = e.clientY - pos.y;
+        switch(contador) {
+            case 1:
+                contenedorUbicaciones.style.transform = 'scale(100%)';
+                disminuir.disabled = true;
+            break;
+            case 2:
+                contenedorUbicaciones.style.transform = 'scale(140%)';
+            break;
+            case 3:
+                contenedorUbicaciones.style.transform = 'scale(165%)';
+                aumentar.disabled = false;
+            break;
+        }
 
-    // Hacer scroll hasta elemento
-    scrollable.scrollTop = pos.top - dy;
-    scrollable.scrollLeft = pos.left - dx;
-}
+        contador -= 1;
+    }
 
-const mouseUpHandler = () => {
-    document.removeEventListener('mousemove', mouseMoveHandler);
-    document.removeEventListener('mouseup', mouseUpHandler);
-    contenedorUbicaciones.style.cursor = 'grab';
-    contenedorUbicaciones.style.removeProperty('user-select');
-}
+    contenedorUbicaciones.addEventListener('dblclick', aumentarZoomConClick);
+    aumentar.addEventListener('click', aumentarZoomConBoton);
+    disminuir.addEventListener('click', disminuirZoomConBoton);
 
-const mouseDownHandler = (e) => {
-    contenedorUbicaciones.style.cursor = 'grabbing';
-    contenedorUbicaciones.style.userSelect = 'none';
-
-    pos = {
-       // Obteniendo scroll actual
-       top: scrollable.scrollTop,
-       left: scrollable.scrollLeft,
-       // Obtener la ubicación actual del mouse
-       x: e.clientX,
-       y: e.clientY,
+    /* * Drag and Scroll */
+    let pos = {
+        top: 0,
+        left: 0,
+        x: 0,
+        y: 0
     };
 
-    document.addEventListener('mousemove', mouseMoveHandler);
-    document.addEventListener('mouseup', mouseUpHandler);
-}
+    const mouseMoveHandler = (e) => {
+        const dx = e.clientX - pos.x;
+        const dy = e.clientY - pos.y;
 
-contenedorUbicaciones.addEventListener('mousedown', mouseDownHandler);
+        // Hacer scroll hasta elemento
+        scrollable.scrollTop = pos.top - dy;
+        scrollable.scrollLeft = pos.left - dx;
+    }
+
+    const mouseUpHandler = () => {
+        document.removeEventListener('mousemove', mouseMoveHandler);
+        document.removeEventListener('mouseup', mouseUpHandler);
+        contenedorUbicaciones.style.cursor = 'grab';
+        contenedorUbicaciones.style.removeProperty('user-select');
+    }
+
+    const mouseDownHandler = (e) => {
+        contenedorUbicaciones.style.cursor = 'grabbing';
+        contenedorUbicaciones.style.userSelect = 'none';
+
+        pos = {
+        // Obteniendo scroll actual
+        top: scrollable.scrollTop,
+        left: scrollable.scrollLeft,
+        // Obtener la ubicación actual del mouse
+        x: e.clientX,
+        y: e.clientY,
+        };
+
+        document.addEventListener('mousemove', mouseMoveHandler);
+        document.addEventListener('mouseup', mouseUpHandler);
+    }
+
+    contenedorUbicaciones.addEventListener('mousedown', mouseDownHandler);
+}
--- resources/views/tiquetera/venta-concierto.blade.php
+++ resources/views/tiquetera/venta-concierto.blade.php
@@ -19,7 +19,8 @@
                             </div>
                             <div class="card-body">
                                 <div id="comprar-boletos">
-                                    <span class="indicacion">Recuerda que puedes realizar únicamente una compra por persona y 8 tickets por compra.</span>
+                                    <span class="indicacion">Recuerda que puedes realizar únicamente una compra por persona
+                                        y 8 tickets por compra.</span>
                                     <hr>
                                     <div class="form-group">
                                         <label class="mdb-main-label" for="localidad">Localidad</label>
@@ -49,7 +50,8 @@
                                     </a>
                                 </div>
                                 <div id="resumen-compra">
-                                    <a class="btn btn-sm btn-block btn-rounded btn-outline-secondary waves-effect" onclick="returnSelectAs()"><i class="fas fa-arrow-circle-left"></i> Volver</a>
+                                    <a class="btn btn-sm btn-block btn-rounded btn-outline-secondary waves-effect"
+                                        onclick="returnSelectAs()"><i class="fas fa-arrow-circle-left"></i> Volver</a>
                                     <div class="d-flex justify-content-between mb-2 mt-4">
                                         <div>
                                             <span id="cantidad-boletos"></span>
@@ -64,7 +66,7 @@
                                         <span>Subtotal</span>
                                         <span id="subTotal"></span>
                                     </div>
-                                  
+
                                     <div class="d-flex justify-content-between mb-2">
                                         <span class="text-success">Total a pagar</span>
                                         <span class="text-success" id="total"></span>
@@ -98,19 +100,23 @@
 
 @section('scripts')
     <script type="text/javascript" src="{{ asset('js/validaciones.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/zoom.js') }}"></script>
     <script type="text/javascript" src="{{ asset('js/boletos.js') }}"></script>
     <script type="text/javascript" src="{{ asset('js/sillas.js') }}"></script>
 
     <script>
-        /////realizamos disparador para cuando la localidad se cambie, se actualice el select de cantidad correspondiendo a la cantidad disponible de esa localidad
+        // Realizamos disparador para cuando la localidad se cambie, se actualice el select de 
+        // cantidad correspondiendo a la cantidad disponible de esa localidad
         $('#localidad').change(function() {
             var id = $('#localidad option:selected').val()
-            
-            if(  id !==undefined){
-            filtrarDisLocalidad(id)
-         }
-        })
-        //// funcion que filtra la cantidad disponible por lo calidad, si tiene disponible igual o mas de 8, el select devolvera de 1 a 8 opciones y si es menor a 8 solo devolvera la cantidad disponible
+
+            if (id !== undefined) {
+                filtrarDisLocalidad(id);
+            }
+        });
+
+        // Función que filtra la cantidad disponible por localidad, si tiene disponible igual o mas de 8, el 
+        // select devolvera de 1 a 8 opciones y si es menor a 8 solo devolvera la cantidad disponible
         function filtrarDisLocalidad(id) {
             $.ajaxSetup({
                 headers: {
@@ -124,7 +130,7 @@
                     $('#filtrarCantidad').html(data);
                     $("#cantidad").materialSelect();
                 }).fail(function(e) {
-                  
+
                     Swal.fire({
                         title: 'Alerta',
                         text: 'A ocurrido un error inesperado',
@@ -134,60 +140,57 @@
                 });
         }
 
-        ////funcion que esta como onclick en el boton volver para regresar a la seleccion de localidades
-        
-        function returnSelectAs()
-        {
+        // Función que esta como onclick en el boton volver para regresar a la seleccion de localidades
+        function returnSelectAs() {
             var comprarBoletos = $('#comprar-boletos');
             var resumenCompra = $('#resumen-compra');
             Swal.fire({
-                title: 'Al regresar se eliminara la localidad seleccionada, ¿Desea continuar?',
-     
+                title: 'Al regresar se eliminará la localidad seleccionada. ¿Desea continuar?',
                 showCancelButton: true,
                 confirmButtonText: 'Continuar',
                 cancelButtonText: `Cancelar`,
-                }).then((result) => {
+            }).then((result) => {
                 /* Read more about isConfirmed, isDenied below */
                 if (result.isConfirmed) {
-                        resetSelect('#cantidad')
-                        resetSelect('#localidad')
-                        $('#localidad').attr('disabled','disabled')
-                         comprarBoletos.fadeIn();
-                         resumenCompra.fadeOut()
-                        $('#vistaLocalidad').html('<div id="vistaLocalidad">'+
-                                    '<img src="{{ asset($evento->imagen_lugar) }}" style="width: 50%">'+
-                                '</div>'+
-                                '<input type="hidden" name="selectSeats2" id="selectSeats2" value="">')
+                    resetSelect('#cantidad')
+                    resetSelect('#localidad')
+                    $('#localidad').attr('disabled', 'disabled')
+                    comprarBoletos.fadeIn();
+                    resumenCompra.fadeOut()
+                    $('#vistaLocalidad').html('<div id="vistaLocalidad">' +
+                        '<img src="{{ asset($evento->imagen_lugar) }}" style="width: 50%">' +
+                        '</div>' +
+                        '<input type="hidden" name="selectSeats2" id="selectSeats2" value="">')
                 } else if (result.isDenied) {
                     Swal.fire('Changes are not saved', '', 'info')
                 }
-                })
+            })
         }
 
-        ///funcion para resetear los material selects
+        // Función para resetear los material selects
         function resetSelect(id) {
             $(id).materialSelect('destroy');
             $(id).val('0').change();
-            if( id == '#cantidad'){
-                $(id).prop('disabled',true)
+            if (id == '#cantidad') {
+                $(id).prop('disabled', true)
             }
             $(id).materialSelect();
         }
 
-        //////funcion para filtrar mapa de localidad si es que la tiene
-
+        // Función para filtrar mapa de localidad si es que la tiene
         function selectAsientos() {
             const localidad = $('#localidad option:selected').val()
             const localidadText = $('#localidad option:selected').text()
-            const localidadIndex = $('#localidad ').index()-1;
+            const localidadIndex = $('#localidad ').index() - 1;
             const cantidad = $('#cantidad option:selected').val();
             const precioUnit = localidades[localidadIndex].precio;
-            const precioUnitDiv  =$('#precioUnit');
+            const precioUnitDiv = $('#precioUnit');
             const subTotal = precioUnit * cantidad;
             const subTotalDiv = $('#subTotal');
             const total = subTotal;
             const totalDiv = $('#total');
             const errores = validandoCamposEntrada();
+
             if (errores == 0) {
                 $.ajaxSetup({
                     headers: {
@@ -200,142 +203,33 @@
                     .done(function(data) {
                         ////se agrega la vista que retorna la funcion ajax al div vistaLocalidad
                         $('#vistaLocalidad').html(data)
-                   
 
-                        const scrollable = document.querySelector('.scrollable');
-                        const contenedorUbicaciones = document.querySelector('.area-zoom');
-                        const aumentar = document.querySelector('#aumentar');
-                        const disminuir = document.querySelector('#disminuir');
+                        // Se guardan los botones de aumento y disminución de zoom
+                        const btnAumentar = document.querySelector('#aumentar');
+                        const btnDisminuir = document.querySelector('#disminuir');
                         ////declaro los div de compra y resumen de boletos
                         var comprarBoletos = $('#comprar-boletos');
                         var resumenCompra = $('#resumen-compra');
                         var cantidadBoletos = $('#cantidad-boletos');
-                        var localidadBoletos= $('#localidad-boletos');
+                        var localidadBoletos = $('#localidad-boletos');
 
-                        let contador = 0;
-
-                        if (aumentar && disminuir) {
-
-                            disminuir.disabled = true;
-
-                            const aumentarZoomConClick = () => {
-                                contador += 1;
-
-                                switch (contador) {
-                                    case 1:
-                                        contenedorUbicaciones.style.transform = 'scale(140%)';
-                                        disminuir.disabled = false;
-                                        break;
-                                    case 2:
-                                        contenedorUbicaciones.style.transform = 'scale(165%)';
-                                        break;
-                                    case 3:
-                                        contenedorUbicaciones.style.transform = 'scale(195%)';
-                                        aumentar.disabled = true;
-                                        break;
-                                    case 4:
-                                        contenedorUbicaciones.style.transform = 'scale(100%)';
-                                        aumentar.disabled = false;
-                                        disminuir.disabled = true;
-                                        contador = 0;
-                                        break;
-                                }
-                            }
-
-                            const aumentarZoomConBoton = () => {
-                                aumentar.disabled = false;
-                                contador += 1;
-
-                                switch (contador) {
-                                    case 1:
-                                        contenedorUbicaciones.style.transform = 'scale(140%)';
-                                        disminuir.disabled = false;
-                                        break;
-                                    case 2:
-                                        contenedorUbicaciones.style.transform = 'scale(165%)';
-                                        break;
-                                    case 3:
-                                        contenedorUbicaciones.style.transform = 'scale(195%)';
-                                        aumentar.disabled = true;
-                                        break;
-                                }
-                            }
-
-                            const disminuirZoomConBoton = () => {
-                                disminuir.disabled = false;
-
-                                switch (contador) {
-                                    case 1:
-                                        contenedorUbicaciones.style.transform = 'scale(100%)';
-                                        disminuir.disabled = true;
-                                        break;
-                                    case 2:
-                                        contenedorUbicaciones.style.transform = 'scale(140%)';
-                                        break;
-                                    case 3:
-                                        contenedorUbicaciones.style.transform = 'scale(165%)';
-                                        aumentar.disabled = false;
-                                        break;
-                                }
-
-                                contador -= 1;
-                            }
-
-                            contenedorUbicaciones.addEventListener('dblclick', aumentarZoomConClick);
-                            aumentar.addEventListener('click', aumentarZoomConBoton);
-                            disminuir.addEventListener('click', disminuirZoomConBoton);
-
-                            /* * Drag and Scroll */
-                            let pos = {
-                                top: 0,
-                                left: 0,
-                                x: 0,
-                                y: 0
-                            };
-
-                            const mouseMoveHandler = (e) => {
-                                const dx = e.clientX - pos.x;
-                                const dy = e.clientY - pos.y;
-
-                                // Hacer scroll hasta elemento
-                                scrollable.scrollTop = pos.top - dy;
-                                scrollable.scrollLeft = pos.left - dx;
-                            }
-
-                            const mouseUpHandler = () => {
-                                document.removeEventListener('mousemove', mouseMoveHandler);
-                                document.removeEventListener('mouseup', mouseUpHandler);
-                                contenedorUbicaciones.style.cursor = 'grab';
-                                contenedorUbicaciones.style.removeProperty('user-select');
-                            }
-
-                            const mouseDownHandler = (e) => {
-                                contenedorUbicaciones.style.cursor = 'grabbing';
-                                contenedorUbicaciones.style.userSelect = 'none';
-
-                                pos = {
-                                    // Obteniendo scroll actual
-                                    top: scrollable.scrollTop,
-                                    left: scrollable.scrollLeft,
-                                    // Obtener la ubicación actual del mouse
-                                    x: e.clientX,
-                                    y: e.clientY,
-                                };
-
-                                document.addEventListener('mousemove', mouseMoveHandler);
-                                document.addEventListener('mouseup', mouseUpHandler);
-                            }
-
-                            contenedorUbicaciones.addEventListener('mousedown', mouseDownHandler);
-                            
+                        // Si ambos botones son diferentes de undefined quiere decir que la localidad
+                        // seleccionada es de tipo Mesa o Silla por lo tanto se debe agregar la funcionalidad
+                        // de hacer zoom y el drag and scroll
+                        if (btnAumentar && btnDisminuir) {
+                            /* 
+                                Se manda a llamar la función zoom que contiene todo el código para que funcione
+                                tanto el Zoom como el drag and scroll, la función está definida en el archivo zoom.js
+                            */
+                            zoom();
                         }
 
                         cantidadBoletos.html(cantidad)
                         localidadBoletos.html(localidadText)
-                        precioUnitDiv.html('$'+precioUnit)
-                        subTotalDiv.html('$'+subTotal.toFixed(2))
-                        totalDiv.html('$'+total.toFixed(2))
-                        /////desaparesco el div de compra de boletos y muestro el resumen
+                        precioUnitDiv.html('$' + precioUnit)
+                        subTotalDiv.html('$' + subTotal.toFixed(2))
+                        totalDiv.html('$' + total.toFixed(2))
+                        // Desaparesco el div de compra de boletos y muestro el resumen
                         comprarBoletos.fadeOut();
                         resumenCompra.fadeIn();
                     });
@@ -343,4 +237,3 @@
         }
     </script>
 @endsection
-
