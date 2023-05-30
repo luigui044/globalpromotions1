d5044ae41971c0073c90d2812a4a398259472603
Roberto Carlos López Abarca <rcla200031@gmail.com>
2023-05-30 14:14:10 -0600

    Eliminando funciones de prueba de VentaController

--- app/Http/Controllers/VentaController.php
+++ app/Http/Controllers/VentaController.php
@@ -6,8 +6,6 @@ use Jenssegers\Date\Date;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use stdClass;
-use App\Models\TmpDetallesVenta;
-use App\Models\TmpVenta;
 use App\Models\TPrueba;
 use App\Models\TEvento;
 use App\Models\VwAsiLocalidade;
@@ -78,61 +76,6 @@ class VentaController extends Controller
         return ['mesa' => $mesa, 'asiento' => $asiento];
     }
 
-    function reservaTemporalEntradas(Request $request) {
-        $ubicacion = $request->ubicacion;
-        $mensaje = '';
-
-        try {
-            $reserva = TmpVenta::updateOrCreate(
-                [
-                    'id_usuario' => 1,
-                    'id_evento' => 1
-                ],
-                [
-                    'precio' => 10
-                ]
-            );
-    
-            if ($ubicacion != "") {
-                $partes = $this->obtenerDetalleUbicacion($ubicacion);
-                $mesa = $partes['mesa'];
-                $asiento = $partes['asiento'];
-                $detalle = new TmpDetallesVenta();
-                $detalle->id_venta = $reserva->id_venta;
-                $detalle->mesa = $mesa;
-                $detalle->asiento = $asiento;
-                $detalle->id_evento = 1;
-                $detalle->save();
-            }
-            $mensaje = 'Registro almacenado correctamente.';
-            return response()->json(['mensaje' => $mensaje]);
-        } catch(Exception $e) {
-            $mensaje = 'Ocurrió un error al guardar el registro: ' . $e->getMessage();
-            return response()->json(['mensaje' => $mensaje], 500);
-        }
-    }
-
-    function eliminarReservaTemporal(Request $request) {
-        $ubicacion = $request->ubicacion;
-        $partes = $this->obtenerDetalleUbicacion($ubicacion);
-        $mesa = $partes['mesa'];
-        $asiento = $partes['asiento'];
-
-        // Se verifica que exista el registro primero
-        $resultado = TmpDetallesVenta::where('mesa', $mesa)
-            ->where('asiento', $asiento)
-            ->first();
-        
-        if (!is_null($resultado)) {
-            $registro = TmpDetallesVenta::where('mesa', $mesa)
-                ->where('asiento', $asiento)
-                ->delete();
-
-            return response()->json(['mensaje' => 'Registro eliminado exitosamente.']);
-        }
-        return response()->json(['mensaje' => 'No se encontró el registro.'], 404);
-    }
-
     function ubicacionDisponible(Request $request) {
         $ubicacion = $request->ubicacion;
         $partes = $this->obtenerDetalleUbicacion($ubicacion);
@@ -155,8 +98,6 @@ class VentaController extends Controller
             return $ubicaciones;
         return response()->json(['mensaje' => 'No se encontró el evento indicado.'], 404);
     }
-
-
 
     function filtrarDisLocalidad(Request $req){
         $localDis = VwAsiLocalidade::where('id_asignacion',$req->id)->first();
